7041a87e74db159cc52cf938fd871bc70b5a58c1
Aditya Waghmare <adityawaghmare31@gmail.com>
2023-09-01 17:42:02 +0530

    [aditya]: check employee present or not

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome");
+            int Is_FULL_TIME = 1;
+            Random random = new Random();   
+            int empCheck=random.Next(0,2);
+            if(empCheck==Is_FULL_TIME) {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+            
+        }
+    }
+}
